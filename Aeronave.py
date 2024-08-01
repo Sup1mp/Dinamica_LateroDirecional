vector operation corrected on derivative dataframe

Aeronave.py
@@ -277,7 +277,7 @@ class Aircraft:
         self.f = fin
         self.t = tail
         self.b = body
-        self.V = V
+        self.V = V #if type(V) == np.ndarray else np.array([V])
 
         return
     
@@ -549,17 +549,23 @@ class Aircraft:
         '''
         Retorna um dataframe com a velocidade e as derivadas
         '''
+        # lists with names and orders
         name = ['V0', 'Lv', 'Lp', 'Lr', 'Le', 'Lc',\
-                 'Nv', 'Np', 'Nr', 'Ne', 'Nc',\
-                      'Yv', 'Yp', 'Yr', 'Ye', 'Yc']
-        deri = [self.V, self.Lv, self.Lp, self.Lr, self.Le, self.Lc,\
-                                    self.Nv, self.Np, self.Nr, self.Ne, self.Nc,\
-                                          self.Yv, self.Yp, self.Yr, self.Ye, self.Yc]
-        dt = DataFrame(columns= name)
-        for i in range(len(name)):
-            dt.loc[:,name[i]] = np.round(deri[i], 4)
+                'Nv', 'Np', 'Nr', 'Ne', 'Nc',\
+                'Yv', 'Yp', 'Yr', 'Ye', 'Yc']
+        order = [self.V, self.Lv, self.Lp, self.Lr, self.Le, self.Lc,\
+                self.Nv, self.Np, self.Nr, self.Ne, self.Nc,\
+                self.Yv, self.Yp, self.Yr, self.Ye, self.Yc]
+        
+        if type(self.V) == np.ndarray:
+            # make sure that the size stays homogeneous even with float inputs
+            deri = np.array(list(map(
+                lambda x: x if type(x) == np.ndarray else np.array([x for _ in range(len(self.V))]),
+                order))).transpose()
+        else:
+            deri = np.array([order])
 
-        return dt
+        return DataFrame(np.round(deri, 4), columns= name)
     
     def curve (self, phi):
         '''

Dinamica_LateroDirecional.py
@@ -43,7 +43,7 @@ class Dinamica_LateroDirecional:
             [0, 0]
         ])
 
-        return self.A, self.B
+        return np.round(self.A, 4), np.round(self.B, 4)
     
     def G (self):
         '''
@@ -169,7 +169,7 @@ class Dinamica_LateroDirecional:
             Npsi_e, Npsi_c
         ]
 
-        return self.delta, self.N
+        return np.round(self.delta, 4), self.N
     
     def aprox_Tr (self):
         '''
@@ -226,12 +226,38 @@ class Dinamica_LateroDirecional:
         wd = np.sqrt(r.real**2 + r.imag**2)
         cd = r.real / wd
 
+        plt.figure()
         plt.scatter(r.real, r.imag, c = 'k')
         plt.grid()
         plt.xlabel('$\sigma$ (rad/s)')
         plt.ylabel('$j \gamma$ (rad/s)')
 
         return wd, cd
+
+    def get_Lf_Sf (self, ro, wd, cd):
+        '''
+        Retorna uma distância Lf e uma área Sf da empenagem vertical baseado nas frequências naturais
+            ro : densidade do ar
+            wd : frequência natural
+            cd : frequência de amortecimento
+        '''
+        # baseado nas equações de aproximação das frequencias
+        A = 2*(wd**2)*self.aero.Iz/(ro*(self.aero.V**2)*self.aero.f.CLa)
+        B = -self.aero.Iz/self.aero.f.CLa * (4*wd*cd*self.aero.m/(ro*self.aero.V) + self.aero.b.Sl*self.aero.b.CDl)
+        delta = sqrt(B**2 + 4*A**2*self.aero.m*self.aero.Iz)
+
+        # distancia do CG até o CA da EV
+        Lf = [
+            (B + delta)/(2*A*self.aero.m),
+            (B - delta)/(2*A*self.aero.m)
+        ]
+
+        # área da EV
+        Sf = [
+            (2*A**2*self.aero.m)/(B + delta),
+            (2*A**2*self.aero.m)/(B - delta)
+        ]
+        return Lf, Sf
 
 if __name__ == "__main__":
     import main

Util.py
@@ -16,10 +16,10 @@ def mach(V, T):
     return V/np.sqrt(1.4*8.31*(T + 273.15)/0.02897)
 
 def erro (real, aprox):
-    return np.where(real!=0, np.round(abs((aprox - real)/real), 3), np.nan)
+    return np.round(np.where(real!=0, abs((aprox - real)/real), abs((aprox - real))), 3)
 
 def erro_dataframe (real: DataFrame, aprox: DataFrame):
-    return abs((aprox - real)/real.replace(0, 1))
+    return np.round(abs((aprox - real)/real.replace(0, 1)), 3)
 
 def weddle (yi, a, b):
     '''
@@ -114,4 +114,15 @@ def read_span (filename):
     return span
 
 if __name__ == "__main__":
-    pass
+    # erro test
+    dn_1 = np.random.random((10))
+    dn_2 = np.array([i for i in range(0, 10)])
+    dn1 = 12.5
+    dn2 = 10
+    print(f"erro_1: {erro(dn_2, dn_1)}")
+    print(f"erro1: {erro(dn2, dn1)}")
+
+    # dataframe error test
+    ddf_1 = DataFrame(dn_1)
+    ddf_2 = DataFrame(dn_2)
+    print(f"erro_dataframe: {erro_dataframe(ddf_2, ddf_1)}")